d5547a3ab55de38013f6f5a30f1c535580c7c433
andronpo <andron663690@mail.ru>
2019-04-03 00:08:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class XMLTagParams {
+	string key;
+	string value;
+public:
+	string stringify() {
+		return " " + key + "=\"" + value + "\"";
+	}
+
+};
+
+class XMLEntity {
+	virtual string stringify() {};
+};
+
+class XMLContent : XMLEntity {
+	string content;
+
+	XMLContent() {}
+
+	XMLContent(string content) {
+		this->content = content;
+	}
+
+	 virtual string stringify() override {
+		 return this->content;
+	}
+};
+
+
+
+
+class XMLTag : XMLEntity {
+	string name;
+	vector <XMLTagParams> params;
+	virtual string stringify_params() {
+		string s;
+		for (XMLTagParams param : params)
+			s += " " + param.stringify();
+		return s;
+	}
+};
+
+
+
+int main()
+{
+    cout << "Hello World!\n"; 
+}

1bb2ed3f635d9f92e6df93eebb27667593a3473a
andronpo <andron663690@mail.ru>
2019-04-04 12:32:37 +0300

    add some constructors

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,10 +3,17 @@
 
 using namespace std;
 
-class XMLTagParams {
+class XMLTagParameter {
 	string key;
 	string value;
 public:
+	XMLTagParameter(string key, string value) {
+		this->key = key;
+		this->value = value;
+	}
+
+	XMLTagParameter() {	}
+
 	string stringify() {
 		return " " + key + "=\"" + value + "\"";
 	}
@@ -15,13 +22,14 @@ public:
 
 class XMLEntity {
 public:
-	virtual string stringify() {};
+	virtual string stringify(){
+		return "foo";
+	};
 };
 
 class XMLContent : public XMLEntity {
+public:
 	string content;
-
-	XMLContent() {}
 
 	XMLContent(string content) {
 		this->content = content;
@@ -35,27 +43,62 @@ class XMLContent : public XMLEntity {
 class XMLTag : public XMLEntity {
 public:
 	string name;
-	vector <XMLTagParams> params;
+	vector<XMLTagParameter> params;
+
+	XMLTag() {}
+
+	XMLTag(string name, vector<XMLTagParameter> params) {
+		this->name = name;
+		this->params = params;
+	}
+	
 	virtual string stringify_params() {
 		string s;
-		for (XMLTagParams param : this->params)
+		for (XMLTagParameter param : this->params)
 			s += " " + param.stringify();
 		return (params.empty() ? "" : s);
 	}
 };
 
 class XMLContentTag : public XMLTag {
-	XMLEntity content;
-	virtual string stringify() override {
-		return ("<" + this->name + this->stringify_params() + ">" + this->content.stringify() + "</" + this->name + ">");
+public:
+	XMLEntity *content;
+	
+	XMLContentTag() {}
+
+	XMLContentTag (string name, vector<XMLTagParameter> params, XMLEntity *content) {
+		this->name = name;
+		this->params = params;
+		this->content = content;
 	}
+
+	virtual string stringify() override {
+		//cout << endl << this->content.stringify() << " " << typeid(this->content).name() << endl;
+		cout << this->stringify_params() << endl;
+		return ("<" + this->name + this->stringify_params() + ">" + this->content->stringify() + "</" + this->name + ">");
+	}
+};
+
+class XMLArray : public XMLEntity {
+
+
 };
 
 class XMLSCTag : public XMLTag {
 
+	XMLSCTag(string name, vector<XMLTagParameter> params) {
+		this->name = name;
+		this->params = params;
+	}
+
+	virtual string stringify() override {
+		return ("<" + this->name + this->stringify_params() + " />");
+	}
 };
 
 int main()
 {
-    cout << "Hello World!\n"; 
+	XMLContent content("CONTENT");
+	cout << XMLContentTag(string("TAG1"), vector<XMLTagParameter> { XMLTagParameter("KEY","VALUE") }, &content).stringify();
+	cout << endl << XMLContent("CONTENT").stringify();
 }

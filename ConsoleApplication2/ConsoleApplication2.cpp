9d2d622f9ac1126d10aa42e07e6125f709c8ec33
andronpo <andron663690@mail.ru>
2019-04-12 00:12:40 +0300

    add XMLCustomTag, WordBody. docx files is working

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -102,6 +102,24 @@ public:
 	}
 };
 
+class XMLCustomTag : public XMLTag {
+public:
+	string prefix, suffix;
+	
+	XMLCustomTag() {}
+
+	XMLCustomTag(string prefix, string name, vector <XMLTagParameter>* params, string suffix) {
+		this->name = name;
+		this->prefix = prefix;
+		this->params = params;
+		this->suffix = suffix;
+	}
+
+	virtual string stringify() override {
+		return ("<" + this->prefix + this->name + this->stringify_params() + this->suffix + ">");
+	}
+
+};
 
 class StringifibleArray : public Stringifible {
 public:
@@ -141,6 +159,7 @@ class WordTemplate : public Stringifible{
 };
 
 class WordBody : public Stringifible {
+public:
 	StringifibleArray* content = new StringifibleArray();
 
 	WordBody() {}
@@ -150,28 +169,61 @@ class WordBody : public Stringifible {
 	}
 
 	virtual string stringify() override {
-		return XMLContentTag("w:document",
-			new vector <XMLTagParameter>{
-				XMLTagParameter("xmlns:wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas"),
-				XMLTagParameter("xmlns:cx", "http://schemas.microsoft.com/office/drawing/2014/chartex"),
-				XMLTagParameter("xmlns:mc", "http://schemas.openxmlformats.org/markup-compatibility/2006"),
-				XMLTagParameter("xmlns:o", "urn:schemas-microsoft-com:office:office"),
-				XMLTagParameter("xmlns:r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships"),
-				XMLTagParameter("xmlns:m", "http://schemas.openxmlformats.org/officeDocument/2006/math"),
-				XMLTagParameter("xmlns:v", "urn:schemas-microsoft-com:vml"),
-				XMLTagParameter("xmlns:wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing"),
-				XMLTagParameter("xmlns:wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"),
-				XMLTagParameter("xmlns:w10", "urn:schemas-microsoft-com:office:word"),
-				XMLTagParameter("xmlns:w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main"),
-				XMLTagParameter("xmlns:w14", "http://schemas.microsoft.com/office/word/2010/wordml"),
-				XMLTagParameter("xmlns:w15", "http://schemas.microsoft.com/office/word/2012/wordml"),
-				XMLTagParameter("xmlns:w16se", "http://schemas.microsoft.com/office/word/2015/wordml/symex"),
-				XMLTagParameter("xmlns:wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup"),
-				XMLTagParameter("xmlns:wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk"),
-				XMLTagParameter("xmlns:wne", "http://schemas.microsoft.com/office/word/2012/wordml"),
-				XMLTagParameter("xmlns:wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape")
-			}
-		).stringify();
+		
+		this->content->add(new XMLContentTag("w:sectPr", 
+			new StringifibleArray(new vector<Stringifible*>{
+				new XMLSelfClosingTag("w:pgSz",
+					new vector <XMLTagParameter> { 
+						XMLTagParameter("w:w", "11906"),
+						XMLTagParameter("w:h", "16838")}),
+				new XMLSelfClosingTag("w:pgMar",
+					new vector <XMLTagParameter> { 
+						XMLTagParameter("w:top", "1134"),
+						XMLTagParameter("w:right", "850"),
+						XMLTagParameter("w:bottom", "1134"),
+						XMLTagParameter("w:left", "1701"),
+						XMLTagParameter("w:header", "708"),
+						XMLTagParameter("w:footer", "708"),
+						XMLTagParameter("w:gutter", "0")}),
+				new XMLSelfClosingTag("w:cols", 
+					new vector <XMLTagParameter> {
+						XMLTagParameter("w:space", "708")}),
+				new XMLSelfClosingTag("w:docGrid",
+					new vector <XMLTagParameter> {
+						XMLTagParameter("w:linePitch", "360")})
+			})));
+
+		return 
+			StringifibleArray(new vector <Stringifible*>{
+				new XMLCustomTag("?", "xml",
+					new vector <XMLTagParameter>{
+						XMLTagParameter("version", "1.0"),
+						XMLTagParameter("encoding", "UTF-8"),
+						XMLTagParameter("standalone", "yes"),
+					}, "?"),
+				new XMLContentTag("w:document",
+					new vector <XMLTagParameter>{
+						XMLTagParameter("xmlns:wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas"),
+						XMLTagParameter("xmlns:cx", "http://schemas.microsoft.com/office/drawing/2014/chartex"),
+						XMLTagParameter("xmlns:mc", "http://schemas.openxmlformats.org/markup-compatibility/2006"),
+						XMLTagParameter("xmlns:o", "urn:schemas-microsoft-com:office:office"),
+						XMLTagParameter("xmlns:r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships"),
+						XMLTagParameter("xmlns:m", "http://schemas.openxmlformats.org/officeDocument/2006/math"),
+						XMLTagParameter("xmlns:v", "urn:schemas-microsoft-com:vml"),
+						XMLTagParameter("xmlns:wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing"),
+						XMLTagParameter("xmlns:wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"),
+						XMLTagParameter("xmlns:w10", "urn:schemas-microsoft-com:office:word"),
+						XMLTagParameter("xmlns:w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main"),
+						XMLTagParameter("xmlns:w14", "http://schemas.microsoft.com/office/word/2010/wordml"),
+						XMLTagParameter("xmlns:w15", "http://schemas.microsoft.com/office/word/2012/wordml"),
+						XMLTagParameter("xmlns:w16se", "http://schemas.microsoft.com/office/word/2015/wordml/symex"),
+						XMLTagParameter("xmlns:wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup"),
+						XMLTagParameter("xmlns:wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk"),
+						XMLTagParameter("xmlns:wne", "http://schemas.microsoft.com/office/word/2012/wordml"),
+						XMLTagParameter("xmlns:wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape")
+					},
+					new XMLContentTag("w:body", content)
+		) }).stringify();
 	}
 };
 
@@ -336,5 +388,8 @@ int main()
 		}).stringify();
 
 	freopen("inde.txt", "w", stdout);
-	cout << WordParagraph("text", true, false, false).stringify();
+	cout << WordBody(new StringifibleArray
+		(new vector <Stringifible*> { 
+			new WordParagraph("text", true, false, false) 
+		})).stringify();
 }

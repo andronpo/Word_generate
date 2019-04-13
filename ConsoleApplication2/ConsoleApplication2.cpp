cead0b0979e706ca4409fa33d937b3ad4e76761c
andronpo <andron663690@mail.ru>
2019-04-13 15:38:50 +0300

    All Complete

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -230,11 +230,79 @@ public:
 class WordChart : public WordTemplate {
 public:
 	string name;
-
+	string id;
+	string size_x;
+	string size_y;
 
 	WordChart() {}
 
-	virtual string stringify() override{}
+	WordChart(string id, string name, string size_x, string size_y) {
+		this->id = id;
+		this->name = name;
+		this->size_x = size_x;
+		this->size_y = size_y;
+	}
+
+	WordChart(string id, string name) : WordChart(id, name, string ("5500000"), string ("3200000")) {}
+
+	virtual string stringify() override{
+		return XMLContentTag("w:p",
+			new XMLContentTag("w:r",
+				new StringifibleArray(new vector <Stringifible*>{
+					new XMLContentTag("w:rPr",
+						new XMLSelfClosingTag("w:noProof")
+					),
+					new XMLContentTag("w:drawing", 
+						new XMLContentTag("wp:inline", 
+							new vector <XMLTagParameter>{
+								XMLTagParameter("distT", "0"),
+								XMLTagParameter("distB", "0"),
+								XMLTagParameter("distL", "0"),
+								XMLTagParameter("distR", "0")
+							},
+							new StringifibleArray(new vector <Stringifible*>{
+								new XMLSelfClosingTag("wp:extent",
+									new vector <XMLTagParameter>{
+										XMLTagParameter("cx", size_x),
+										XMLTagParameter("cy", size_y)
+									}),
+								
+								new XMLSelfClosingTag("wp:effectExtent",
+									new vector <XMLTagParameter>{
+										XMLTagParameter("l", "0"),
+										XMLTagParameter("t", "0"),
+										XMLTagParameter("r", "19050"),
+										XMLTagParameter("b", "19050")
+									}),
+
+								new XMLSelfClosingTag("wp:docPr",
+									new vector <XMLTagParameter>{
+										XMLTagParameter("id", name),
+										XMLTagParameter("name", "Chart " + name)
+									}),
+
+								new XMLSelfClosingTag("wp:cNvGraphicFramePr"),
+
+								new XMLContentTag("a:graphic",
+									new vector <XMLTagParameter>{
+										XMLTagParameter("xmlns:a", "http://schemas.openxmlformats.org/drawingml/2006/main")
+									},
+									new XMLContentTag("a:graphicData",
+										new vector <XMLTagParameter>{
+											XMLTagParameter("uri", "http://schemas.openxmlformats.org/drawingml/2006/chart"),
+										},
+										new XMLSelfClosingTag("c:chart",
+											new vector <XMLTagParameter>{
+												XMLTagParameter("xmlns:c", "http://schemas.openxmlformats.org/drawingml/2006/chart"),
+												XMLTagParameter("xmlns:r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships"),
+												XMLTagParameter("r:id", id),
+											})
+									)
+
+								)
+							})))
+				}))).stringify();
+	}
 };
 
 class WordParagraph : public WordTemplate {
@@ -389,7 +457,8 @@ int main()
 
 	freopen("inde.txt", "w", stdout);
 	cout << WordBody(new StringifibleArray
-		(new vector <Stringifible*> { 
-			new WordParagraph("text", true, false, false) 
+	(new vector <Stringifible*>{
+			new WordParagraph("text", true, false, false),
+			new WordChart("rId5", "1")
 		})).stringify();
 }
